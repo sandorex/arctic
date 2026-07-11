d6657a1d905157bb3307c2cd8577279edd2a99c4
sandorex <rzhw3h@gmail.com>
2026-07-11 22:25:44 +0200

    Working menu with encoder
    Finally got the encoder working, interrupts are fricking evil man

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,175 @@
 #define PORT_IR PORTB
 #define PIN_IR 9
 
-#define PORT_ENC PORTD
-#define PIN_ENC_BTN 17  // A3 (INT1)
-#define PIN_ENC_BTN_INT 1
-#define PIN_ENC1 16     // A2 (INT0)
-#define PIN_ENC1_INT 0
-#define PIN_ENC2 15     // A1
-
 #define SCREEN_ADDRESS 0x3C
-
-#define INT8_MAX 127
 
 #include "ir_codes.h"
 #include <Arduino.h>
 #include <Wire.h>
 #include <stdint.h>
+#include <RotaryEncoder.h>
+#include <OneButton.h>
 
 #define SSD1306_NO_SPLASH // do not show splashcreen
 #include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+
+enum Menu {
+    ON_TIME,
+    ENABLED,
+    OFF_TIME,
+    TEST,
+
+    // how many items there are
+    LENGTH
+};
+
 // by default it uses A5 and A4 for SCK/SDA
-Adafruit_SSD1306 display(128, 32, &Wire, -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-volatile int8_t value = 0;
-volatile int8_t oldValue = 0;
-volatile bool button = false;
+RotaryEncoder encoder(4, 7, RotaryEncoder::LatchMode::FOUR3);
 
-void encoder();
-void encoder_button();
+OneButton btn = OneButton(
+    2,           // Input pin for the button
+    true,        // Button is active LOW
+    true         // Enable internal pull-up resistor
+);
+
+int pos = 0;
+int menu_index = 0;
+bool menu_editing = false;
+bool enabled = true;
+bool air_conditioner = false;
+
+#define MIN_ON_TIME 5
+#define MAX_ON_TIME 15
+
+#define MIN_OFF_TIME 10
+#define MAX_OFF_TIME 45
+
+uint8_t on_time = 5;
+uint8_t off_time = 25;
 
 void setup() {
+    Serial.begin(9600);
+
     // wait for display
     delay(500);
 
-    display.begin();
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        for(;;);
+    }
 
     // dim to lower power consumption
     display.dim(true);
 
+    // TODO boot splash
+
     display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println(F("Hello"));
     display.display();
 
-    // make all encoder pins inputs
-    PORT_ENC &= ~(_BV(PIN_ENC1) | _BV(PIN_ENC2) | _BV(PIN_ENC_BTN));
+    pinMode(13, OUTPUT);
 
-    // NOTE as ATMega328p has only two interrupts i am using one for the button
-    // and other for one of the encoder data pins as the other one will have a
-    // diode to trigger the same interrupt
-    attachInterrupt(PIN_ENC_BTN_INT, encoder_button, CHANGE);
-    attachInterrupt(PIN_ENC1_INT, encoder, RISING);
+    // attachInterrupt(digitalPinToInterrupt(2), encoder_button, CHANGE);
 }
 
-void loop() {
-    if (value != oldValue) {
-        if (value > oldValue) {
-            // plus
-        } else {
-            // minus
-        }
-    } else if (button) {
-        // lone button press
-    }
+// TODO add version somewhere on screen
+void updateScreen() {
+    display.clearDisplay();
 
-    delay(5);
-}
-
-void encoder() {
-    // reset the value so it does not overflow
-    if (abs(value) == INT8_MAX) {
-        oldValue = 0;
-        value = 0;
-    }
-
-    // second pin is HIGH then it triggered the interrupt
-    if (PORT_ENC & _BV(PIN_ENC2)) {
-        value += 1;
+    if (menu_index == Menu::TEST && menu_editing) {
+        // simple test screen
+        display.setCursor(43, 2);
+        display.println("IR TEST");
+        display.setCursor(10, 17);
+        display.println("< OFF         ON >");
     } else {
-        value -= 1;
+        // TODO simple indicator that user is editing option
+        if (menu_editing) {
+            display.fillCircle(1, 1, 1, 1);
+        }
+
+        // top right corner
+        display.setCursor(90, 0);
+        display.printf("AC %s", air_conditioner ? "ON" : "OFF");
+
+        // interval on field
+        display.setCursor(3, 13);
+        display.print("ON");
+        if (menu_index == Menu::ON_TIME) {
+            display.drawRect(0, 10, 17, 13, 1);
+        }
+
+        display.setCursor(0, 25);
+        display.printf("%dm", on_time);
+
+        // enable field (center) DO NOT MODIFY BY HAND
+        display.setCursor(46, 13);
+        display.println("ENABLE");
+        if (menu_index == Menu::ENABLED) {
+            display.drawRect(43, 10, 41, 13, 1);
+        }
+
+        display.setCursor(58, 25);
+        display.println(enabled ? "ON" : "OFF");
+
+        // interval off field
+        display.setCursor(108, 12);
+        display.print("OFF");
+        if (menu_index == Menu::OFF_TIME) {
+            display.drawRect(105, 9, 23, 13, 1);
+        }
+
+        display.setCursor(110, 25);
+        display.printf("%dm", off_time);
     }
+
+    display.display();
 }
 
-void encoder_button() {
-    button = (PORT_ENC & _BV(PIN_ENC_BTN));
+bool led = false;
+void loop() {
+    encoder.tick();
+    btn.tick();
+
+    int newPos = encoder.getPosition();
+    if (pos != newPos) {
+        pos = newPos;
+
+        if (menu_editing) {
+            switch (menu_index) {
+                case Menu::ON_TIME:
+                    on_time = min(max(on_time + (int)encoder.getDirection(), MIN_ON_TIME), MAX_ON_TIME);
+                    break;
+                case Menu::ENABLED:
+                    enabled = !enabled;
+                    break;
+                case Menu::OFF_TIME:
+                    off_time = min(max(off_time + (int)encoder.getDirection(), MIN_OFF_TIME), MAX_OFF_TIME);
+                    break;
+                case Menu::TEST: // TODO
+                    break;
+            }
+
+        } else {
+            menu_index = min(max(menu_index + (int)encoder.getDirection(), 0), Menu::LENGTH - 1);
+        }
+
+        updateScreen();
+    } else {
+        int clicks = btn.getNumberClicks();
+        if (clicks > 0) {
+            menu_editing = !menu_editing;
+            led = !led;
+            digitalWrite(13, led);
+
+            updateScreen();
+        }
+    }
 }
